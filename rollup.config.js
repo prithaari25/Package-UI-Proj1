636851f5d4863ef1412fea9f868bca082004701d
prithaari25 <aripritha1998@gmail.com>
2024-03-18 00:06:20 +0530

    Initial

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     output: {
         dir: "dist",
         format: "es",
-        name: "package-to-ui",
+        name: "package-ui-proj1",
     },
     external: ["react", "react-dom"],
     plugins: [typescript({ tsconfig: "tsconfig.json" })],
