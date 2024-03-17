0dc03959ed559cd6c3fbbe29340cd4644e0da368
prithaari25 <aripritha1998@gmail.com>
2024-03-17 23:34:29 +0530

    Initial commit

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     output: {
         dir: "dist",
         format: "es",
-        name: "pack-to-ui",
+        name: "package-to-ui",
     },
     external: ["react", "react-dom"],
     plugins: [typescript({ tsconfig: "tsconfig.json" })],
